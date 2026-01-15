ef907e08307b5b74c0cbc34f0bb0803bf54b9f3a
Anna Kessler <kessler.anna96@gmail.com>
2026-01-15 18:41:38 +0300

    Фиксы

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 import { initHeader } from './modules/header/header.js';
 import { initSliders } from './modules/sliders/sliders.js';
+import './modules/modal/modal.js';
 
 initHeader();
 initSliders();
